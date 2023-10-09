182f2c87d8391d83ea889627cf3c6046817d8173
Anton Ozerov <lamerork@gmail.com>
2023-10-09 11:23:27 +0300

    Add frontend

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 
 def show_index(request):
-     return render(request, 'index.html')
+    template = loader.get_template('index.html')
+    context = {}
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
