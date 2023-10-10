9b6162afd62385e4595ad8c2477161321a155674
Anton Ozerov <lamerork@gmail.com>
2023-10-10 21:25:17 +0300

    Add WYSWYG editor

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=100)
     description_short = models.TextField('Краткоен описание', blank=True)
-    description_long = models.TextField('Описание', blank=True)
+    description_long = HTMLField('Описание', blank=True)
     lng = models.FloatField()
     lat = models.FloatField()
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,22 +1,7 @@
-"""where_to_go URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from .views import show_index, show_detail
 
 
@@ -25,4 +10,5 @@ urlpatterns = [
     path('', show_index),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('places/<int:id>', show_detail, name='place_detail'),
+    path('tinymce/', include('tinymce.urls')),
 ]
